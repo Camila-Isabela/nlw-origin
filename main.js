2961a6b582f411c56a902b60c699b044eb306351
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-05 11:04:10 -0300

    logic menu

--- main.js
+++ main.js
@@ -8,3 +8,11 @@ function onScroll() {
     }
 }
 
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
